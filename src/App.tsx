16c9cbc60c30c1e4728f90885ac9f694e59308db
shaula <shaulajasminetya22@gmail.com>
2022-08-26 21:48:41 +0700

    Add configs images and icons, edit README.md

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
 import React from "react";
+import { ICONS, IMAGES } from "./configs";
 
 function App() {
   return (
     <div className="App">
       <header className="flex">
-        <p>text</p>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Learn React
-        </a>
+        <img alt="" className="w-full h-20" src={IMAGES.AMERICANEXPRESS} />
+        <img alt="" className="w-full h-20" src={ICONS.GIFT} />
       </header>
     </div>
   );
---
+++ src/configs/icons.ts
@@ -0,0 +1,11 @@
+import GIFT from "../assets/icons/ic-gift.svg";
+import SHOPPINGBAG from "../assets/icons/ic-shopping-bag.svg";
+import TRUCK from "../assets/icons/ic-truck.svg";
+
+const icons = {
+  GIFT,
+  SHOPPINGBAG,
+  TRUCK,
+};
+
+export default icons;
